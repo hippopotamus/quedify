3534d8a1eb936249daeb9520cf7db3b0f0538191
Armen Vartan <vartan.armen@gmail.com>
2015-06-13 12:10:03 -0400

    first test on a route

--- test/meetings.js
+++ test/meetings.js
@@ -1,7 +1,23 @@
-var assert = require('assert')
+var assert = require('assert');
+var http = require('http');
+var app = require('../app.js');
+var server = http.createServer(app);
 
-describe('the answer', function(){
-  it('should equal 42', function(){
-    assert.equal(42, 42)
+describe('meetings unit tests:', function(){
+  before(function(){
+    server.listen(3001)
+  });
+
+  after(function(){
+    server.close()
   })
-})
+
+  describe('GET /meetings', function(){
+    it('should return a status code of 200', function(done){
+      http.get('http://127.0.0.1:3001/meetings', function(res){
+        assert.equal(res.statusCode, 200);
+        done();
+      });
+    });
+  });
+});
