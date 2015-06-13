b1adf9184926e03a4ac3ed6a405bcf0a583f781d
Armen Vartan <vartan.armen@gmail.com>
2015-06-13 00:10:45 -0400

    cleaning up. moving 500 and 404 code to routes.js

--- app/routes.js
+++ app/routes.js
@@ -14,4 +14,28 @@ module.exports = function(app){
   app.use(function (req, res, next) {
     res.status(404).send("404");
   });
+
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  });
 }
