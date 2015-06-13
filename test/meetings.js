444e5fb96bd69fcfa283883c37f77906bff23f17
Armen Vartan <vartan.armen@gmail.com>
2015-06-13 12:46:17 -0400

    model tests plus an empty db test to start the site

--- test/meetings.js
+++ test/meetings.js
@@ -3,16 +3,61 @@ var http = require('http');
 var app = require('../app.js');
 var server = http.createServer(app);
 
-describe('meetings unit tests:', function(){
-  before(function(){
-    server.listen(3001)
-  });
+var Meeting = require('../app/models/meeting.js')
 
-  after(function(){
-    server.close()
+describe('meetings', function(){
+  describe('meeting model:', function(){
+    after(function(){
+      Meeting.collection.remove()
+    })
+
+    it('should create without error', function(done){
+      var meeting = new Meeting({
+        title: "visiting mom",
+        from: "2015-04-25",
+        to: "2015-04-27",
+        location: "mom's house",
+        description: "visiting momma!",
+        participants: "mom, me, poppa"
+      })
+
+      meeting.save(function (err, meeting){
+        if (err){ throw err; }
+        else{
+          done()
+        }
+      });
+    })
+
+    it('should save participants as an array', function(done){
+      Meeting.findOne({"title": "visiting mom"}, function(err, meeting){
+        if (err){ throw err; }
+        // assert.equal(meeting.from, Date("2015-04-25"))
+        assert.equal(meeting.participants[0], "mom")
+        assert.equal(meeting.participants[1], "me")
+        assert.equal(meeting.participants[2], "poppa")
+        done()
+      })
+    })
   })
 
   describe('GET /meetings', function(){
+    before(function(){
+      server.listen(3001)
+    });
+
+    after(function(){
+      server.close()
+      Meeting.collection.remove()
+    })
+
+    it('should be an empty collection', function(done){
+      Meeting.find(function(err, meetings){
+        if (err){ throw err }
+        assert.equal(meetings.length, 0)
+        done()
+      })
+    })
     it('should return a status code of 200', function(done){
       http.get('http://127.0.0.1:3001/meetings', function(res){
         assert.equal(res.statusCode, 200);
