a668ffa8002ad93dc145403d08515aea3b3677a7
Armen Vartan <vartan.armen@gmail.com>
2015-06-13 16:55:25 -0400

    added route for homepage

--- config/routes.js
+++ config/routes.js
@@ -3,13 +3,17 @@ var router = express.Router();
 var meetings = require('../app/controllers/meetings.js')
 
 module.exports = function(app){
-  app.get('/meetings', meetings.index)
+  app.get('/', function(req, res, next) {
+    res.render('index');
+  });
+
+  app.get('/meetings', meetings.index);
   app.get('/meetings/new', meetings.new);
-  app.post('/meetings/', meetings.create)
-  app.get('/meetings/:id', meetings.show)
-  app.get('/meetings/:id/edit', meetings.edit)
-  app.put('/meetings/:id', meetings.update)
-  app.delete('/meetings/:id', meetings.delete)
+  app.post('/meetings/', meetings.create);
+  app.get('/meetings/:id', meetings.show);
+  app.get('/meetings/:id/edit', meetings.edit);
+  app.put('/meetings/:id', meetings.update);
+  app.delete('/meetings/:id', meetings.delete);
 
   app.use(function (req, res, next) {
     res.status(404);
@@ -28,7 +32,7 @@ module.exports = function(app){
         error: err
       });
     });
-  }
+  };
 
   // production error handler
   // no stacktraces leaked to user
