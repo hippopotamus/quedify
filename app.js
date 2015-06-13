a1bb9ca3bd6adbfe76288433789cf7ce03cfc828
Armen Vartan <vartan.armen@gmail.com>
2015-06-12 20:15:36 -0400

    added mongodb to app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// init mongo
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
