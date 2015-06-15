ffae3e0e89252bee738d275197ace54f97474232
Armen Vartan <vartan.armen@gmail.com>
2015-06-14 21:44:54 -0400

    dumb mistake

--- config/settings.js
+++ config/settings.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   "testing": {
     "dbConnectionString": "localhost:27017/quedifytesting"
-  }
+  },
   "production": {
     "dbConnectionString": "mongodb://assessment:assessmentEvents2014@ds037977.mongolab.com:37977/events"
   },
